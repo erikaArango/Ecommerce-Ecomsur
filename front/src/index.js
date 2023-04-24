27fd173b5f85d57f47e0bf835356b50872facfa3
erikaArango <erikaarango.developer@gmail.com>
2023-04-24 16:21:16 -0400

    creating redux store

--- front/src/index.js
+++ front/src/index.js
@@ -3,9 +3,22 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+
+import productsReducer from "./slices/productsSlice";
+
+const store = configureStore ({
+  reducer: {
+    products: productsReducer
+  }
+})
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
