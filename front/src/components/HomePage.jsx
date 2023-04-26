7a27861a6da4bd3f15b5f82b01d4e97acaf82702
erikaArango <erikaarango.developer@gmail.com>
2023-04-25 23:28:04 -0500

    styles HomePage component

--- front/src/components/HomePage.jsx
+++ front/src/components/HomePage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { productsApi, useGetAllProductsQuery } from "../slices/productsApi";
+import {  useGetAllProductsQuery } from "../slices/productsApi";
 
 const HomePage = () => {
      const {data, error, isLoading} = useGetAllProductsQuery();
@@ -12,15 +11,15 @@ const HomePage = () => {
                         <p>An error ocurred</p> 
                   ) : (
                         <>
-                              <h2>NEW ARRIVALS</h2>
-                              <div className="productos">
+                              <h2>THE BEST TECHNOLOGY</h2>
+                              <div className="products">
                                     {data?.map( (product) => (
                                           <div key={product._id} className="product">
                                                 <h3>{product.name}</h3>
                                                 <img src={`http://localhost:5000/${product.image}`} alt={product.name}/>
                                                 <div className="details">
-                                                      <span>{product.description}</span>
                                                       <span className="price">${product.price}</span>
+                                                      <span>{product.description}</span>
                                                 </div>
                                                 <button>Add to Cart</button>
                                           </div>
